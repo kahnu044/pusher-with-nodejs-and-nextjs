5430dc11569fb705b4d57b87266203a5a0c4c2c1
kahnu044 <kahnucharan044@gmail.com>
2024-07-26 17:58:50 +0530

    Pusher updated to backend server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const Pusher = require('pusher');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+    // useTLS: true
+});
 
 app.get('/', (req, res) => {
     res.json({
@@ -11,6 +20,21 @@ app.get('/', (req, res) => {
     });
 });
 
+app.post('/api/pusher/auth', (req, res) => {
+    const socketId = req.body.socket_id;
+    const channel = req.body.channel_name;
+    const auth = pusher.authenticate(socketId, channel);
+    res.send(auth);
+});
+
+app.get('/api/messages', (req, res) => {
+    const message = req.body.message;
+    pusher.trigger('chat-app-test', 'test-event', {
+        message: message,
+        time: new Date()
+    });
+    res.send({ status: 'Message sent' });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
